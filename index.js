f622b749c3dd9c50208465cd849f37459936ca27
Sydney Michael <m13s19madden@gmail.com>
2020-04-03 17:13:22 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function breakOut(arr, chVal, stVal) {
+  for (lei i = 0, i < arr.length -1, i++) {
+    if (arr[i] === stVal) {
+      break
+    }
+    arr[i] = chVal
+  }
+  return arr
+}
+
+function keepGoing(arr, chVal, skVal) {
+  
+}
+
+function findBy(arr, findFn) {
+  
+}
